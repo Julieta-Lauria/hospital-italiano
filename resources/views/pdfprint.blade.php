d278b555becbcad3d20a776e220e148345271981
Pablo Traverso <jpablo.traverso@gmail.com>
2019-10-14 11:00:56 -0300

    Print pdf css

--- resources/views/pdfprint.blade.php
+++ resources/views/pdfprint.blade.php
@@ -4,29 +4,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ ltrim(public_path('css/ppdf.css'), '/') }}" />
     <title>Reporte Informativo hospital italiano</title>
-    <?php $options_columns = ['Afirmaciones', 'Nada identificada', 'Poco identificada', 'Muy identificada'] ?>
+    <?php $options_columns = ['Opciones sin mover', 'Nada identificada', 'Poco identificada', 'Muy identificada'] ?>
   </head>
   <body>
 
         <main>
 
-          <div class="container-fluid-tablet">
-            <div class="navbar-header">
+          <div class="navbar-header">
 
-              <img src="img/logo-hospital.jpg" alt="" class="logo-hospital" style = "height: 90px;">
-              <img src="img/logo-fundacion.jpg" alt="" class="logo-fundacion" style = "height: 90px;">
+              <img src="img/logo-hospital.jpg" alt="" class="logo-hospital">
+              <img src="img/logo-fundacion.jpg" alt="" class="logo-fundacion">
 
-              <ul class="nav navbar-nav ">
-                <h2>Herramienta para la toma de decisiones compartidas</h2>
-              </ul>
+              <h2 class="headertitle">Herramienta para la toma de decisiones compartidas</h2>
 
-              </div>
-            </div>
+          </div>
+
 
 
           <div id="details" class="clearfix">
             <div id="invoice">
-              <h1>Reporte {{ $invoice }}</h1>
+              <h1>Informe {{ $invoice }}</h1>
               <div class="date">Fecha: {{ $date }}</div>
             </div>
           </div>
@@ -34,7 +31,7 @@
           <div class="">
             <h2>Grafico de ayuda:</h2>
               <div class="grafico2pdf">
-                <img src="img/graf2.png" style = "width: 550px;">
+                <img src="img/graf2.png" class="graf2impresion">
               </div>
           </div>
 
@@ -42,12 +39,9 @@
           <div class="">
             <h2>Grafico de ayuda:</h2>
               <div class="grafico3pdf">
-                <img src="img/graf3.png" style = "width: 550px;">
+                <img src="img/graf3.png" class="graf3impresion">
               </div>
           </div>
-
-
-
 
 
           <div class="">
