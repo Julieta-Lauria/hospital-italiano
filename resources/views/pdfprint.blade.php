eabdaf1c25f9d665ea6ad305d07f837e21f29ec7
Julieta-Lauria <julietalauria@gmail.com>
2019-12-07 14:46:54 -0300

    Actualizado graficos y textos del pdf

--- resources/views/pdfprint.blade.php
+++ resources/views/pdfprint.blade.php
@@ -42,27 +42,28 @@
         </div> -->
 
 <br>
-          <h2>Infografia con riesgos y beneficios</h2>
+          <h2>Infografía con riesgos y beneficios</h2>
+          <br>
             <div class="grafico1pdf">
-              @if ($edad > 55)
-              <img src="img/grafico2000.png" style = "width: 700px;" style = "height: 400px;">
+              @if ($edad < 50)
+              <img src="img/grafico-pdf-40-1.PNG" style = "width: 700px;" style = "height: 400px;">
+              <img src="img/grafico-pdf-40-2.PNG" style = "width: 700px;" style = "height: 400px;">
               @else
-              <h2> Hola Mundao </h2>
+              <img src="img/grafico-pdf-50-1.PNG" style = "width: 700px;" style = "height: 400px;">
+              <img src="img/grafico-pdf-50-2.PNG" style = "width: 700px;" style = "height: 400px;">
               @endif
             </div>
-
-
-<br>
+<br><br><br>
+<br><br><br>
+<br><br><br>
             <h2>Definiciones</h2>
               <div class="definiciones">
-                <p>Falsas alarmas:<br><br>Cuando la mamografía muestra una anomalía que podría ser cáncer, hay que hacer otros estudios para aclarar. Estos estudios pueden ser desde una ecografía, otra mamografía con una técnica distinta, y hasta una biopsia (tomar una muestra del tejido de la mama). Si se demuestra que la anomalía era benigna, decimos que fue una falsa alarma.</p>
-                <p>Sobrediagnóstico:<br><br>Algunos tipos de cánceres de mama que se detectan por el tamizaje crecen tan lento que nunca llegarían a ser un problema de salud. Algunos inclusive desaparecen solos sin tratamiento. Hasta el momento no tenemos forma de diferenciar estos tipos de cánceres de los que realmente dañan a la mujer, por los que se ofrece tratamiento a todos los diagnósticos de cáncer de mama. Esto quiere decir que algunas mujeres con un cáncer de mama detectado por el tamizaje pueden recibir tratamientos innecesarios. Esto se conoce como sobrediagnóstico y sobretratamiento.</p><br>
+                <p><strong>Falsas alarmas:</strong><br><br>Cuando la mamografía muestra un hallazgo que aumenta el nivel de sospecha y motiva a realizar más estudios hasta que finalmente se determina que se trata de una condición benigna. Estos estudios pueden ser desde una ecografía, otra mamografía con una técnica distinta, y hasta una biopsia (tomar una muestra del tejido de la mama). Algunas mujeres expresan que estas falsas alarmas les generan ansiedad y miedo.</p>
+                <p><strong>Sobrediagnóstico:</strong><br><br>Algunos tipos de cánceres de mama que se detectan por el tamizaje crecen tan lento que nunca llegarían a ser un problema de salud. Algunos inclusive desaparecen solos sin tratamiento. Hasta el momento no tenemos forma de diferenciar estos tipos de cánceres de los que realmente dañan a la mujer, por los que se ofrece tratamiento a todos los diagnósticos de cáncer de mama. Esto quiere decir que algunas mujeres con un cáncer de mama detectado por el tamizaje pueden recibir tratamientos innecesarios. Esto se conoce como sobrediagnóstico y sobretratamiento.</p><br>
               </div>
 
 
-              <br><br><br><br><br>
-              <br><br><br><br><br>
-              <br><br><br><br>
+              <br>
 
             <h2>Tabla de decisiones:</h2>
             <br>
